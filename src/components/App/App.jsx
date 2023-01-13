5e228f0e1a40f6e108b81358fb61fb881b0b3a79
Natalie McMann <nmcmann110@gmail.com>
2023-01-13 16:27:55 -0600

    pseudo-coding my useEffect and get route

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  //make some useState
+
+  //make a useEffect to get initial array state
+
+  //make a get route to call on and pass down
+
     return (
       <div className="App">
         <header className="App-header">
