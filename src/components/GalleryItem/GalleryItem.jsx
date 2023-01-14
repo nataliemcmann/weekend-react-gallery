f7345b152db8b51860d15b8e953d377438f80920
Natalie McMann <nmcmann110@gmail.com>
2023-01-14 09:33:40 -0600

    set up conditional render that toggles between photo description and image upon click

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,12 +1,25 @@
 import { useState } from 'react';
 
 function GalleryItem ({picture}) {
+    const [click, setClick] = useState(false);
 
-    return (
-        <div>
-        <img src={picture.path} width="250" height="200"/> 
-        </div>
-    )
+    const handleClick = () => {
+        setClick(!click);
+    }
+
+    if (click === false) {
+        return (
+            <div onClick={handleClick}>
+            <img src={picture.path} width="250" height="200"/> 
+            </div>
+        )
+    } else {
+        return (
+            <div onClick={handleClick}>
+                <p>{picture.description}</p>
+            </div>
+        )
+    }
 }
 
 export default GalleryItem;
